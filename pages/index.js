99e363a3ff443bf2099c2c847dadc5fb37f53b17
jawkneefence <jawkneefence@gmail.com>
2023-02-26 13:46:59 -0800

    search limit set to 1008 dex entries

--- pages/index.js
+++ pages/index.js
@@ -13,20 +13,23 @@ export default function Home({monList, fullDex}) {
 
   //onClick function for prev/next buttons
   //url encoded in api response through {pokemon.previous/next}
+  //stops at dex entry #1008
   const fetchPokemon = async (url, next) => {
     const response = await fetch(url);
     const nextMon = await response.json();
     setOffset(next ? offset + offsetAmount : offset - offsetAmount)
+    if(offset > 911) {
+      nextMon.next = false;
+    }
     setPokemon(nextMon)
   }
-  let pageTracker = offsetAmount;
 
   return (
       <Layout title={"Pokédex"}>
         <div className="flex flex-row justify-center">
         <button disabled={!pokemon.previous} className="rounded-md h-9 disabled:bg-gray-500 px-3 py-1 mt-3 bg-slate-300" onClick={() => fetchPokemon(pokemon.previous, false)}>Prev</button>
         <SearchMons dex = {fullDex}></SearchMons>
-      <button disabled={!pokemon.next && pageTracker} className="rounded-md h-9 disabled:bg-gray-500 px-3 py-1 mt-3 bg-slate-300" onClick={() => fetchPokemon(pokemon.next, true)}>Next</button>
+      <button disabled={!pokemon.next} className="rounded-md h-9 disabled:bg-gray-500 px-3 py-1 mt-3 bg-slate-300" onClick={() => fetchPokemon(pokemon.next, true)}>Next</button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10">
         {pokemon.results.map((mon, i) => (
