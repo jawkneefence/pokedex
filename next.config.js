78350c36793b31b540eee02841e3227520c712e1
jawkneefence <jawkneefence@gmail.com>
2023-02-26 12:56:04 -0800

    undo image deoptimization

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['assets.pokemon.com', 'img.pokemondb.net', 'cdn-icons-png.flaticon.com'],
-    unoptimized: true
+    domains: ['assets.pokemon.com', 'img.pokemondb.net', 'cdn-icons-png.flaticon.com']
   }
 }
 
